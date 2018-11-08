906c506d74eca4c93f400652b85a98686426ceb1
sean <ccomett@gmail.com>
2018-11-08 00:53:58 -0800

    Delete LoadingBaseMvpPresenter

--- app/src/main/java/com/chuntingyu/weather/applications/LoadingPresenterBase.java
+++ app/src/main/java/com/chuntingyu/weather/applications/LoadingPresenterBase.java
@@ -6,13 +6,12 @@ import com.chuntingyu.weather.tools.coredata.DataManager;
  * Created by Kevin on 2018/3/14.
  */
 
-public class LoadingPresenterBase<V extends LoadingMvpView> extends BasePresenterBase<V> implements LoadingBaseMvpPresenter<V> {
+public class LoadingPresenterBase<V extends LoadingMvpView> extends BasePresenterBase<V> {
 
     public LoadingPresenterBase(DataManager dataManager) {
         super(dataManager);
     }
 
-    @Override
     public void choiceActivity() {
         if (getDataManager().getLoggedInMode()) {
             getMvpView().startMain();
